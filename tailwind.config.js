e9d8eed8c078d1ce2f51ed491df3fba697c46ba8
wilobimerchandise-rgb <wilobimerchandise@gmail.com>
2026-08-02 10:17:26 +0100

    Fix formatting in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {
-        brand: { 500: '#F97316', 600: '#EA580C' }
-      }
+      colors: { brand: { 500: '#f97316', 600: '#ea580c' } }
     },
   },
   plugins: [],
