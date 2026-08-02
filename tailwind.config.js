23ade1c304ada0b98e151f617a01679fe8d638bd
wilobimerchandise-rgb <wilobimerchandise@gmail.com>
2026-08-02 08:23:49 +0100

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: { 500: '#F97316', 600: '#EA580C' }
+      }
+    },
+  },
+  plugins: [],
+}
