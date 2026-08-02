4f41b708b4d22996705c926e15e2456a01ea5a96
wilobimerchandise-rgb <wilobimerchandise@gmail.com>
2026-08-02 09:27:47 +0100

    Update comment for base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/Eby-Gold-inv-New/' // IMPORTANT: must match repo name
+  base: '/Eby-Gold-inv-New/' // MUST match your repo name with capitals
 })
