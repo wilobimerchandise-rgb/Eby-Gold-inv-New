428bd24ad30fdaef91ecf4fa093e5047bf59c741
wilobimerchandise-rgb <wilobimerchandise@gmail.com>
2026-08-02 10:16:28 +0100

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Eby-Gold-inv-New/' // MUST match your repo name with capitals
+  base: '/Eby-Gold-inv-New/'
 })
